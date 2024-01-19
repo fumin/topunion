f4380c531fc44a7aec9d08610ced4c5c0b2553ff
fumin <awawfumin@gmail.com>
2024-01-19 20:39:21 +0800

    onvif working

---
+++ shuicao/cmd/onvif/main.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"context"
+	"encoding/xml"
+	"flag"
+	"io"
+	"log"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/use-go/onvif"
+	onvifdevice "github.com/use-go/onvif/device"
+	onvifmedia "github.com/use-go/onvif/media"
+	onvifsdkmedia "github.com/use-go/onvif/sdk/media"
+	onvifxsd "github.com/use-go/onvif/xsd/onvif"
+)
+
+func setIP(ctx context.Context, device *onvif.Device) error {
+	getResp, err := device.CallMethod(onvifdevice.GetNetworkInterfaces{})
+	if err != nil {
+		return errors.Wrap(err, "")
+	}
+	defer getResp.Body.Close()
+	b, err := io.ReadAll(getResp.Body)
+	if err != nil {
+		return errors.Wrap(err, "")
+	}
+	interfaces := struct {
+		Header struct{}
+		Body   struct {
+			GetNetworkInterfacesResponse struct {
+				NetworkInterfaces struct {
+					Token   onvifxsd.ReferenceToken `xml:"token,attr"`
+					Enabled bool
+					IPv4    struct {
+						Enabled bool
+						Config  struct {
+							Manual struct {
+								Address      string
+								PrefixLength int
+							}
+							LinkLocal struct {
+								Address      string
+								PrefixLength int
+							}
+							FromDHCP struct {
+								Address      string
+								PrefixLength int
+							}
+							DHCP bool
+						}
+					}
+				}
+			}
+		}
+	}{}
+	if err := xml.Unmarshal(b, &interfaces); err != nil {
+		return errors.Wrap(err, "")
+	}
+	log.Printf("%#v", interfaces)
+	ni := interfaces.Body.GetNetworkInterfacesResponse.NetworkInterfaces
+
+	// setInterface := onvifdevice.SetNetworkInterfaces{}
+	setInterface := struct {
+		XMLName          string                  `xml:"tds:SetNetworkInterfaces"`
+		InterfaceToken   onvifxsd.ReferenceToken `xml:"tds:InterfaceToken"`
+		NetworkInterface struct {
+			Enabled bool `xml:"onvif:Enabled"`
+			IPv4    struct {
+				Enabled bool `xml:"onvif:Enabled"`
+				DHCP    bool `xml:"onvif:DHCP"`
+			} `xml:"onvif:IPv4"`
+		} `xml:"tds:NetworkInterface"`
+	}{}
+	setInterface.InterfaceToken = ni.Token
+	setInterface.NetworkInterface.Enabled = true
+	setInterface.NetworkInterface.IPv4.Enabled = true
+	setInterface.NetworkInterface.IPv4.DHCP = true
+	log.Printf("%#v", setInterface)
+	setResp, err := device.CallMethod(setInterface)
+	if err != nil {
+		return errors.Wrap(err, "")
+	}
+	defer setResp.Body.Close()
+	setB, err := io.ReadAll(setResp.Body)
+	if err != nil {
+		return errors.Wrap(err, "")
+	}
+	log.Printf("%s", setB)
+
+	return nil
+}
+
+func getStreamUri(ctx context.Context, device *onvif.Device) (string, error) {
+	getResp, err := device.CallMethod(onvifmedia.GetProfiles{})
+	if err != nil {
+		return "", errors.Wrap(err, "")
+	}
+	defer getResp.Body.Close()
+	b, err := io.ReadAll(getResp.Body)
+	if err != nil {
+		return "", errors.Wrap(err, "")
+	}
+	log.Printf("%s", b)
+	profiles := struct {
+		Header struct{}
+		Body   struct {
+			GetProfilesResponse onvifmedia.GetProfilesResponse
+		}
+	}{}
+	if err := xml.Unmarshal(b, &profiles); err != nil {
+		return "", errors.Wrap(err, "")
+	}
+	log.Printf("%#v", profiles)
+	profile := profiles.Body.GetProfilesResponse.Profiles[1]
+
+	stream, err := onvifsdkmedia.Call_GetStreamUri(ctx, device, onvifmedia.GetStreamUri{ProfileToken: profile.Token})
+	if err != nil {
+		return "", errors.Wrap(err, "")
+	}
+	log.Printf("%#v", stream)
+	uri := string(stream.MediaUri.Uri)
+	return uri, nil
+}
+
+func main() {
+	flag.Parse()
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds | log.Llongfile)
+
+	if err := mainWithErr(); err != nil {
+		log.Fatalf("%+v", err)
+	}
+}
+
+func mainWithErr() error {
+	xaddr := "192.168.0.4:80"
+	username := "admin"
+	password := "123456"
+	device, err := onvif.NewDevice(onvif.DeviceParams{Xaddr: xaddr, Username: username, Password: password})
+	if err != nil {
+		return errors.Wrap(err, "")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	// if err := setIP(ctx, device); err != nil {
+	// 	return errors.Wrap(err, "")
+	// }
+
+	uri, err := getStreamUri(ctx, device)
+	if err != nil {
+		return errors.Wrap(err, "")
+	}
+	log.Printf("%#v", uri)
+
+	return nil
+}
--- shuicao/go.mod
+++ shuicao/go.mod
@@ -4,16 +4,22 @@ go 1.21.5
 
 require (
 	github.com/bluenviron/gortsplib/v4 v4.6.2
+	github.com/pion/rtp v1.8.3
 	github.com/pkg/errors v0.9.1
+	github.com/use-go/onvif v0.0.9
 )
 
 require (
+	github.com/beevik/etree v1.1.0 // indirect
 	github.com/bluenviron/mediacommon v1.5.1 // indirect
+	github.com/elgs/gostrgen v0.0.0-20161222160715-9d61ae07eeae // indirect
+	github.com/gofrs/uuid v3.2.0+incompatible // indirect
 	github.com/google/uuid v1.4.0 // indirect
+	github.com/juju/errors v0.0.0-20220331221717-b38fca44723b // indirect
 	github.com/pion/randutil v0.1.0 // indirect
 	github.com/pion/rtcp v1.2.12 // indirect
-	github.com/pion/rtp v1.8.3 // indirect
 	github.com/pion/sdp/v3 v3.0.6 // indirect
+	github.com/rs/zerolog v1.26.1 // indirect
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 )
